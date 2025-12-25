af511da17f5e709e3c260abe6e949c19f1715805
aohus <dbtnghk528@gmail.com>
2025-12-25 21:35:32 +0900

    fix(storage): remove unused S3StorageService from exports

--- src/app/storage/__init__.py
+++ src/app/storage/__init__.py
@@ -1,6 +1,5 @@
 from .base import StorageService
 from .local import LocalStorageService
-from .s3 import S3StorageService
 from .factory import get_storage_client
 
-__all__ = ["StorageService", "LocalStorageService", "S3StorageService", "get_storage_client"]
+__all__ = ["StorageService", "LocalStorageService", "get_storage_client"]
